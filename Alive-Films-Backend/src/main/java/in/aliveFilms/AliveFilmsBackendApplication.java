2c4185e14d6517f78e8dc2b2af195666bbba7ada
Rohan Bankar <96434805+RohanB39@users.noreply.github.com>
2024-08-29 21:20:19 +0530

    Alive Films Project Start

---
+++ Alive-Films-Backend/src/main/java/in/aliveFilms/AliveFilmsBackendApplication.java
@@ -0,0 +1,13 @@
+package in.aliveFilms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AliveFilmsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AliveFilmsBackendApplication.class, args);
+	}
+
+}
