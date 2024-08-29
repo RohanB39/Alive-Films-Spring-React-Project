20124adcd8724ef53f4608e9430f9bdb1bf9fa94
Rohan Bankar <96434805+RohanB39@users.noreply.github.com>
2024-08-29 21:39:41 +0530

    Connection File ready

--- Alive-Films-Backend/src/main/java/in/aliveFilms/dbConn/ConnectionManager.java
+++ Alive-Films-Backend/src/main/java/in/aliveFilms/dbConn/ConnectionManager.java
@@ -5,18 +5,17 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionManager {
-    private static String url = "jdbc:mysql://localhost:3306/prototypeeop";
-    private static String driverName = "com.mysql.jdbc.Driver";
+    private static String url = "jdbc:mysql://localhost:3306/alivefilms";
     private static String username = "root";
-    private static String password = "triala";
+    private static String password = "25082018@Rt";
     private static Connection con;
-    private static String urlstring;
 
     public static Connection getConnection() {
         try {
+            String driverName = "com.mysql.cj.jdbc.Driver";
             Class.forName(driverName);
             try {
-                con = DriverManager.getConnection(urlstring, username, password);
+                con = DriverManager.getConnection(url, username, password);
             } catch (SQLException ex) {
                 // log an exception. fro example:
                 System.out.println("Failed to create the database connection.");
